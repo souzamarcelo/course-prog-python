1d44597b08505e41335dbeb9c718961a5f7a56c8
Marcelo <marcelo.desouza@udesc.br>
2022-10-04 20:13:06 -0300

    Update exercises.

--- 3-exercicios/src/4-funcoes/triangulos.py
+++ 3-exercicios/src/4-funcoes/triangulos.py
@@ -10,8 +10,23 @@ def tipo_triangulo(x, y, z):
 
 import sys
 
+qtd_equilatero = 0
+qtd_isosceles = 0
+qtd_escaleno = 0
+
 for i in range(3, 16, 3):
     x = int(sys.argv[i - 2])
     y = int(sys.argv[i - 1])
     z = int(sys.argv[i])
-    print(x, y, z, '---', tipo_triangulo(x, y, z))
+    tipo = tipo_triangulo(x, y, z)
+    print(x, y, z, '---', tipo)
+    
+    if tipo == 'TRIÂNGULO EQUILÁTERO': qtd_equilatero += 1
+    if tipo == 'TRIÂNGULO ISÓSCELES': qtd_isosceles += 1
+    if tipo == 'TRIÂNGULO ESCALENO': qtd_escaleno += 1
+    
+print()
+print('QUANTIDADES:')
+print('Equilátero:', qtd_equilatero)
+print('Isósceles:', qtd_isosceles)
+print('Escaleno:', qtd_escaleno)
