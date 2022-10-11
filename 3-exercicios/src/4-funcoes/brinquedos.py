244b846ae352b1e0e6364964b78182c00fe1f601
Marcelo <marcelo.desouza@udesc.br>
2022-10-11 17:53:09 -0300

    Update exercises.

--- 3-exercicios/src/4-funcoes/brinquedos.py
+++ 3-exercicios/src/4-funcoes/brinquedos.py
@@ -1,43 +1,57 @@
 import math
 
-def leitura():
-    a = int(input('Informe o valor da primeira dimensão: '))
-    if a == -1:
-        return None
-    b = int(input('Informe o valor da segunda dimensão: '))
-    c = int(input('Informe o valor da terceira dimensão: '))
-    return [a, b, c]
+qtd_10 = 0
+qtd_15 = 0
+qtd_20 = 0
+qtd_25 = 0
+maiores = 0
 
-def calcula_d(a, b, c):
-    return math.sqrt((a ** 2) + (b ** 2) + (c ** 2))
+def diametro(a, b, c):
+    return math.sqrt(a ** 2 + b ** 2 + c ** 2)
 
-esferas_10 = 0
-esferas_15 = 0
-esferas_20 = 0
-esferas_25 = 0
-maior_25 = 0
-
-dados = leitura()
-while dados != None:
-    a = dados[0]
-    b = dados[1]
-    c = dados[2]
-    d = calcula_d(a, b, c)
-    if d > 25:
-        maior_25 += 1
-    elif d > 20:
-        esferas_25 += 1
-    elif d > 15:
-        esferas_20 += 1
-    elif d > 10:
-        esferas_15 += 1
+def esfera():
+    a = float(input('Informe a dimensão A: '))
+    if a <= 0:
+        mostra_resultado()
+    
+    b = float(input('Informe a dimensão B: '))
+    c = float(input('Informe a dimensão C: '))
+    
+    D = diametro(a, b, c)
+    
+    if D <= 10:
+        return '10'
+    elif D <= 15:
+        return '15'
+    elif D <= 20:
+        return '20'
+    elif D <= 25:
+        return '25'
     else:
-        esferas_10 += 1
-
-    dados = leitura()
-
-print(f'Esferas 10cm: {esferas_10}')
-print(f'Esferas 15cm: {esferas_15}')
-print(f'Esferas 20cm: {esferas_20}')
-print(f'Esferas 25cm: {esferas_25}')
-print(f'Caixas com diagonal maior que 25cm: {maior_25}')
+        return None
+    
+    
+def mostra_resultado():
+    print()
+    print('RESULTADO:')
+    print(f'Esferas de tamanho 10: {qtd_10}')
+    print(f'Esferas de tamanho 15: {qtd_15}')
+    print(f'Esferas de tamanho 20: {qtd_20}')
+    print(f'Esferas de tamanho 25: {qtd_25}')
+    print(f'Total de esferas: {qtd_10 + qtd_15 + qtd_20 + qtd_25}')
+    print(f'Caixas que não cabem em nenhuma esfera: {maiores}')
+    exit()
+    
+    
+while True:
+    e = esfera()
+    if e == '10':
+        qtd_10 += 1
+    elif e == '15':
+        qtd_15 += 1
+    elif e == '20':
+        qtd_20 += 1
+    elif e == '25':
+        qtd_25 += 1
+    else:
+        maiores += 1
